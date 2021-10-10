68a6c1190db75508612a1ee5482a3a99cf4368af
Mikko Rajakangas <mikko.rajakangas@visma.com>
2021-10-10 16:45:07 +0300

    added domain model, app level functions

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,39 @@
+import { Product} from "./domain/product.type"
+import { Drinker, Gender } from "./domain/drinker.type"
+import { BacData } from "./domain/bac-data.type"
+
 export class App {
-  public message = 'Hello World!';
+  public products: Product[] = []
+  public drinker: Drinker = {
+    gender: Gender.Male,
+    weight: 80,
+    time: 1
+  }
+  public result: string
+
+  updateProductList(list: Product[]): void {
+    this.products = list
+  }
+
+  updateDrinkerParams(data: Drinker): void {
+    this.drinker = data
+  }
+
+  async calculateBAC(): Promise<void> {
+    const data: BacData = {
+      products: this.products,
+      drinker: this.drinker
+    }
+
+    const bac = await fetch(`http://localhost:3000/api/bac`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    console.log(bac)
+  }
 }
